4d53db0dd1c8f3bd2846a1dfe64c47fb75cdd0a0
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-06-29 09:49:00 -0300

    Update TratinTheory.js

--- TratinTheory.js
+++ TratinTheory.js
@@ -15,8 +15,21 @@ var currency;
 var init = () => {
     currency1 = theory.createCurrency();
     currency2 = theory.createCurrency();
+
+    ///////////////////
+    // Regular Upgrades
+
+    // v1
+    {
+        let getDesc = (level) => "v_1=" + getV1(level).toString(0);
+        v1 = theory.createUpgrade(0, currency1, new FirstFreeCost(new ExponentialCost(5, Math.log2(2))));
+        v1.getDescription = (_) => Utils.getMath(getDesc(v1.level));
+        v1.getInfo = (amount) => Utils.getMathTo(getDesc(v1.level), getDesc(v1.level + amount));
+    }
 }
 
 var getTau = () => currency1.value;
+
+var getV1 = (level) => Utils.getStepwisePowerSum(level, 5.5, 8, 0);
 
 init();
