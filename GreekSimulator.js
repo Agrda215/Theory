b4379101243a1784837825d0c54bf3d0dc26c265
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-07-16 16:34:32 -0300

    cool

---
+++ GreekSimulator.js
@@ -0,0 +1,15 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "greek";
+var name = "Greek Simulator";
+var description = "===CHANGELOG=== \nv1.0.0:\nImproved A & B";
+var authors = "Agrda251";
+var version = "1.0.0";
+
+var init = () => {
+    
+}
